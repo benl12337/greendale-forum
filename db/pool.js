3f48a0a93cdba0fe2e78605c11aeb38d60800528
Ben <luben12337@gmail.com>
2025-01-22 22:14:10 +1100

    Changed database variable name

--- db/pool.js
+++ db/pool.js
@@ -1,6 +1,6 @@
 const { Pool } = require("pg");
 
-console.log('the  database conenction is', process.env.DB_URL);
+console.log('the  database conenction is', process.env.DATABASE_URL);
 
 module.exports = new Pool({
     connectionString: process.env.DB_URL,
